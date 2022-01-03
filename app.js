bc76650085309f224f3bea6cecfba510d025cc6a
CamAveryH <camheath0@gmail.com>
2022-01-03 10:21:03 -0500

    Created a function for the final results

--- app.js
+++ app.js
@@ -180,3 +180,123 @@ function findSibling(entry) {
   }
 }
 
+function findParents(entry) {
+  let i = 0;
+  let filteredParents = [];
+  do {    
+      people.filter(function (person) {
+          if(person.id === entry[i]) {
+          filteredParents.push(person);
+              return true;
+          }
+          return false;
+      });
+      i += 1;
+  } while(i < entry.length); 
+  if(filteredParents.length > 0) {
+      return filteredParents;
+  }else{
+      alert("This person appeared on this earth magically, and does not have parents.")
+  }
+
+}
+
+function findKids(entry) {
+  let i = 0;
+  let filteredKids = [];
+  do {
+      people.filter(function (person) {
+          if(person.parents[i] === entry.id) {
+              filteredKids.push(person);
+                  return true;
+              }
+              return false;
+      });
+      i += 1;
+  } while(i < 2);
+  if(filteredKids.length > 0) {
+      return filteredKids;
+      }else{
+          alert("This person does not have any children.");
+      }
+}
+
+let btnGet = document.querySelector('button');
+let myTable = document.querySelector('#table');
+
+btnGet.addEventListener('click', () => {
+let results = completeSearch();
+  if(results.length < 2) {
+      let person = results[0]
+      let parents = intersect(person.parents, people.id)
+      let spouse = findSpouse(person);
+      let siblings = findSibling(person);
+      let kids = findKids(person);
+      parents = (findParents(parents));
+      let finalResultsArray = [];
+      finalResultsArray.push(person);
+      finalResultsArray = finalResultsArray.concat(spouse,siblings,kids,parents);
+      console.log(finalResultsArray);
+      
+
+  let headers = ['ID','First Name', 'Last Name', 'Gender', 'Eye Color', 'DOB', 'Height', 'Weight', 'Occupation', 'Parent', 'Spouse'];
+  let table = document.createElement('table');   
+  let titleRow = document.createElement('tr');
+
+  headers.forEach(titleText => {
+      let title = document.createElement('th');
+      let textNode = document.createTextNode(titleText);
+      title.appendChild(textNode);
+      titleRow.appendChild(title);
+     
+  });
+table.appendChild(titleRow);
+
+finalResultsArray.forEach(persons => {
+  let row = document.createElement('tr');
+
+  Object.values(persons).forEach(text => {
+      let cell = document.createElement('td');
+      let textNode = document.createTextNode(text);
+      cell.appendChild(textNode);
+      row.appendChild(cell);
+  })
+
+  table.appendChild(row);
+})
+
+myTable.appendChild(table);
+      
+
+      
+      return;
+  }
+  else {
+  let headers = ['ID','First Name', 'Last Name', 'Gender', 'Eye Color', 'DOB', 'Height', 'Weight', 'Occupation', 'Parent', 'Spouse'];
+  let table = document.createElement('table');   
+  let titleRow = document.createElement('tr');
+
+  headers.forEach(titleText => {
+      let title = document.createElement('th');
+      let textNode = document.createTextNode(titleText);
+      title.appendChild(textNode);
+      titleRow.appendChild(title);
+     
+  });
+table.appendChild(titleRow);
+
+results.forEach(person => {
+  let row = document.createElement('tr');
+
+  Object.values(person).forEach(text => {
+      let cell = document.createElement('td');
+      let textNode = document.createTextNode(text);
+      cell.appendChild(textNode);
+      row.appendChild(cell);
+  })
+
+  table.appendChild(row);
+})
+
+myTable.appendChild(table);
+}});
